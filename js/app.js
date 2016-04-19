a24b27ffa027ba47ad2ba0bce478c0a0c6f2b97f
Tila <cjsgamer@gmail.com>
2016-04-19 11:40:29 -0700

    Added router

--- js/app.js
+++ js/app.js
@@ -4,6 +4,10 @@ app.config(['$routeProvider', function($routeProvider) {
    $routeProvider.
    
    when('/', {
+      templateUrl: 'pages/home.html', controller: 'test'
+   }).
+
+   when('/apps', {
       templateUrl: 'pages/apps.html', controller: 'test'
    }).
 
