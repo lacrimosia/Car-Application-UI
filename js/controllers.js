a0a3bf3db6c642025beb196753dc5072c20d80ae
Shatilla Prayer <prayers@unlv.nevada.edu>
2016-04-20 11:34:28 -0700

    Added icons to main menu

--- js/controllers.js
+++ js/controllers.js
@@ -12,17 +12,25 @@ app.controller('home', [
     	$scope.name = $scope.data.user;               // user's name
     	$scope.userThumb = $scope.data.userThumb;     // user thumbnail image
     	$scope.deleted = false;         // Checks if shortcut is deleted
+    	$scope.backup = [];             // when deleted, goes here in case of undo or reset
 
     	// remove desktop shortcut on click
     	$scope.deleteBox = function(index){
     		$scope.selected = index;
     		// modalService.openMenuModal('t', title, description);
-    		$scope.shortcutList.splice($scope.selected, 1);
+    		if($scope.selected != -1){
+    			$scope.shortcutList.splice($scope.selected, 1);
+    			$scope.backup.push($scope.shortcutList[$scope.selected]);
+    			console.log($scope.backup);
+    		}else{
+    			console.log("Nothing to delete");
+    		}
+    		
     	};
     });
 
 }]);
 
-app.controller('apps', ['scope', '$http', function($scope, $http){
+app.controller('apps', ['$scope', '$http', function($scope, $http){
 
 }]);
